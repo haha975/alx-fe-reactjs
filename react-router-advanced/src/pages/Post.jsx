07a9babc8e5d4cb613bcd2b4c7a5f391de1ccd81
habeshe <yfreheiwot@gmail.com>
2025-10-08 23:32:06 +0300

    Create Post.jsx

---
+++ react-router-advanced/src/pages/Post.jsx
@@ -0,0 +1,8 @@
+// src/pages/Post.jsx
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function Post() {
+  const { id } = useParams();
+  return <h2>Viewing Post ID: {id}</h2>;
+}
