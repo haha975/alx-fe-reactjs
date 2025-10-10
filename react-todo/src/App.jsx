d4daca3c58abcfe104b79825e19b99bd33671081
habeshe <yfreheiwot@gmail.com>
2025-10-10 17:37:07 +0300

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -3,8 +3,7 @@ import TodoList from "./components/TodoList";
 
 export default function App() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>React Todo App</h1>
+    <div>
       <TodoList />
     </div>
   );
